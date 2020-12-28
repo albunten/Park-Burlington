234efd1636a260b46fdb1c2187de396dd68557e8
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-12-28 13:38:42 -0500

    try adding attributes to api key

--- Public/api.js
+++ Public/api.js
@@ -1,20 +1,25 @@
 let script = document.createElement('script')
 
-fetch('./apikey')
-  .then((response) => {
-    return response.text()
-  }).then((apiKey) => {
-        let googleReq = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap`;
-    script.setAttribute('src', googleReq)
-    script.setAttribute('async', true)
-    script.setAttribute('defer', true)
-    document.body.appendChild(script)
-     return script
+ fetch('./apiKey')
+   .then((response) => {
+      return response.text()
+   }).then((apiKey) => {
      
-  })
+      let googleReq = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap`;
+    
+      script.setAttribute('src', googleReq)
+      script.setAttribute('async', true)
+      script.setAttribute('defer', true)
+      script.setAttribute('type', "text/javascript")
+      document.body.appendChild(script)
+      // console.log(script)
+      return script
+
+   })
 
 
 
 
-
+//    < script async defer src = "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+// type = "text/javascript" ></script >
 
